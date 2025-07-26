35086af3b658e6d41c0b4273e13c19cfcc0de7a8
prince1565 <kumar.prince832002@gmail.com>
2025-07-26 11:15:11 +0530

    after set base url

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'todobackend-production-d227.up.railway.app';
+export const API_BASE_URL ='todobackend-production-d227.up.railway.app';
